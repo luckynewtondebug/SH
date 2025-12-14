48056b57d471b4bbb0a5947351cb70aaafa6a7f6
Luckynewton <luckynewton63@gmail.com>
2025-12-13 22:11:22 -0600

    working lua

--- Contents/mods/MyMod1/42.12/media/lua/shared/MyMod1gato/script.lua
+++ Contents/mods/MyMod1/42.12/media/lua/shared/MyMod1gato/script.lua
@@ -1,2 +1,13 @@
-local cell = getCell()
-local zombies = cell:getZombieList()
+
+local function OnKeyPressed(key)
+    if key == Keyboard.KEY_T then
+        local cell = getCell()
+        local zombies = cell:getZombieList()
+        for i = 0, zombies:size() - 1 do
+            local zombie = zombies:get(i)
+            zombie:addLineChatElement("Hello World!")
+        end
+    end
+end
+
+Events.OnKeyPressed.Add(OnKeyPressed)
